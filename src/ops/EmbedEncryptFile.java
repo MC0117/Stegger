c4eafd46e58f5dfc202498478ced5923381fb248
MC0117 <manfred.clase@gmail.com>
2021-10-12 12:14:06 +0200

    Added Main.java and EmbedEncryptFile.java classes

---
+++ src/ops/EmbedEncryptFile.java
@@ -0,0 +1,16 @@
+package ops;
+import java.lang.String;
+
+public class EmbedEncryptFile {
+    private void embedToFile(String message, String backgroundFile, String key){
+    }
+    protected int genHopValue(String formattedMessage, String backgroundFile, String key){
+        return 0;
+    }
+    protected String formattedMessage(String message, String key){
+        return "";
+    }
+    protected boolean feasibilityCheck(String formattedMessage, String backgroundFile){
+        return true;
+    }
+}
